cba53289700b7a0f8307245f869a6f5e014388e0
Shashank Singh <shashank9830@users.noreply.github.com>
2017-12-20 01:20:31 +0530

    Updated Factorial.java for testing

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,6 +1,7 @@
 public class Factorial {
 
     //Recursive function to calculate factorial
+    //Change comment
     private static int factorial(int n) {
         if (n == 0)
             return 1;
