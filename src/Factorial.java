d70b5c5ad93a1260c75fd061dcb68978851e7c48
Shashank Singh <shashank9830@users.noreply.github.com>
2017-12-20 01:19:15 +0530

    Initial commit

---
+++ src/Factorial.java
@@ -0,0 +1,21 @@
+public class Factorial {
+
+    //Recursive function to calculate factorial
+    private static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return (n * factorial(n-1));
+    }
+
+    //Main function
+    public static void main(String [] args) {
+
+        int fact;
+        int number = 4;
+
+        fact = factorial(number);
+
+        System.out.println("\n Factorial of " + number + " is: " + fact);
+    }
+}
